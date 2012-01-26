5f35679e9b9f15952bc367b15ba11c1bccb360e0
Patrick <firefly2442@gmail.com>
2012-01-25 21:19:54 -0600

    use jquery to show folder contents when clicked

--- index.php
+++ index.php
@@ -15,17 +15,27 @@ function recursiveDirectory($path = '.', $level = 0) {
 		//Ignore directories back
 		if ($file != "." && $file != "..") {
 
-			$spaces = str_repeat('&nbsp;', ($level * 4));
-
 			if (is_dir( "$path/$file")) {
 				//It's a directory, continue reading
-				echo "<img src='images/folder.png' alt='Folder' title='Folder'><strong>$spaces $file</strong><br>\n";
+				//div id's can't have periods or other weird characters so we generate a unique ID
+				$unique_id = uniqid("unique_");
+				echo "<div id='folder'><div id=\"folder_".$unique_id."\"><img src='images/folder.png' alt='Folder' title='Folder'>$file</div></div><br>\n";
+				echo "<div id=\"".$unique_id."\"><div id='box'>\n";
 				recursiveDirectory("$path/$file", ($level+1));
+				echo "</div></div>\n";
+
+				echo "<script>\n";
+				//hide all folder contents by default
+				echo "$(\"#".$unique_id."\").hide();\n";
+
+				echo "$(\"#folder_".$unique_id."\").click(function () {\n";
+				echo "$(\"#".$unique_id."\").show('fast');\n";
+				echo "});</script>\n";
 			} else {
 				//Make sure it's an approved extension
 				if (in_array(substr($file, -4), $media)) {
 					$folder = str_replace(MEDIA_PATH, "", $path);
-					echo "$spaces<a href=\"create.php?media=".$folder."/".$file."\"/>".$file."</a><br>\n";
+					echo "<a href=\"create.php?media=".$folder."/".$file."\">".$file."</a><br>\n";
 				}
 			}
 		}
@@ -41,6 +51,7 @@ function recursiveDirectory($path = '.', $level = 0) {
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="./javascript/jquery-1.7.1.min.js"></script>
 	<link rel="shortcut icon" href="favicon.ico" >
 	<title>php-live-transcode</title>
 </head>
